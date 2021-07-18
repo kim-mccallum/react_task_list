673af713e563eef409c3a98530cedac040df690b
Kimchee <kchaase@gmail.com>
2021-07-18 12:04:17 -0600

    added card component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,36 @@ import { useState, Fragment } from "react";
 import DragAndDropList from "./Components/DragAndDropList";
 import Header from "./Components/Layout/Header";
 import Menu from './Components/Layout/Menu';
+import Card from './Components/UI/Card/Card';
+
+// The list:
+const TODOS = [
+  {
+    id: "1",
+    name: "Feed the Boots",
+    thumb: "/images/check.png",
+  },
+  {
+    id: "2",
+    name: "Feed the Boots more treats",
+    thumb: "/images/star.png",
+  },
+  {
+    id: "3",
+    name: "Buy the Boots more toys",
+    thumb: "/images/star.png",
+  },
+  {
+    id: "4",
+    name: "Stretch",
+    thumb: "/images/check.png",
+  },
+  {
+    id: "5",
+    name: "Stretch some more",
+    thumb: "/images/star.png",
+  },
+];
 
 function App() {
   const [ menuShown, setMenuShown ] = useState(false);
@@ -19,9 +49,11 @@ function App() {
     <Fragment>
       <Header onShowMenu={showMenuHandler}/>
       {menuShown && <Menu onHideMenu={hideMenuHandler} />}
-      <h1>Simple Drag and Drop List </h1>
-
-      <DragAndDropList />
+      <main>
+        <Card>
+          <DragAndDropList toDoList={TODOS}/>
+        </Card>
+      </main>      
     </Fragment>
   );
 }
--- src/Components/Layout/Menu.js
+++ src/Components/Layout/Menu.js
@@ -7,6 +7,7 @@ const Menu = props => {
     return (
         <Modal onCloseMenu={props.onHideMenu}>
             <h2>MENU</h2>
+            <p>this will eventually have links to login/logout or add a new task...</p>
             <button className={classes.button} onClick={props.onHideMenu}>HIDE MENU</button>
         </Modal>
     )
