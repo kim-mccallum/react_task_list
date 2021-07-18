cb59eb469fd625cc97da59cee30bcd8082953b68
Kimchee <kchaase@gmail.com>
2021-07-18 12:27:30 -0600

    added header background image

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import headerImage from '../../assets/header-img.jpg'; 
 
 import classes from "./Header.module.css";
 
@@ -6,9 +7,12 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>TaskLister1</h1>
+        <h1>TaskLister</h1>
         <button className={classes.button} onClick={props.onShowMenu}>SHOW MENU</button>
       </header>
+      <div className={classes['main-image']}>
+            <img src={headerImage} alt="A background of books"/>
+        </div>
     </Fragment>
   );
 };
